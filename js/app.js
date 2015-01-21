a9cb2dccc0aae9b22e5622a6d83237b5ab84e794
rm913 <rachelmakovoz@gmail.com>
2015-01-21 14:36:16 -0500

    k

--- js/app.js
+++ js/app.js
@@ -3,10 +3,17 @@
 $(document).ready(function(){	
 		//run on document ready (can also run on button click)
 		$('#submit').click(function(){	
-			var num = $('#number').val();
+			var num = +($('#number').val());
+			if(num === NaN){
+					alert("Please enter a number!");
+				};
+			
+
+
+			
+		
 			for (var i = 1; i <= num; i++ ) {
-				
-				 if (i % 5 === 0 && i % 3 === 0) {	// use === for checking type equality
+				if (i % 5 === 0 && i % 3 === 0) {	// use === for checking type equality
 					$('.answer ul').append('<li>'+ i + ": Fizz-Buzz" + '</li>');
 				}
 				else if (i % 3 === 0) {
@@ -15,10 +22,16 @@ $(document).ready(function(){
 				else if (i % 5 === 0) {			
 					$('.answer ul').append('<li>'+ i +  ": Buzz" + '</li>');
 				}
+
 				else {
 					$('.answer ul').append('<li>'+ i + " : " + i + '</li>');
 				}
+			
+
 			}
+
+			
+
 		});
 
 
