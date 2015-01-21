dadd5b99c9aa4b084d53ef7cd9faf679fe60e419
rm913 <rachelmakovoz@gmail.com>
2015-01-21 13:57:12 -0500

    h

--- js/app.js
+++ js/app.js
@@ -3,10 +3,9 @@
 $(document).ready(function(){	
 		//run on document ready (can also run on button click)
 		$('#submit').click(function(){	
-			for (var i = 1; i <= 100; i++ ) {
-				//print the number in the console; press F12 to fire console
-				console.log("Loop number ", i);
-				//alignment: tab size 4
+			var num = $('#number').val();
+			for (var i = 1; i <= num; i++ ) {
+				
 				 if (i % 5 === 0 && i % 3 === 0) {	// use === for checking type equality
 					$('.answer ul').append('<li>'+ i + ": Fizz-Buzz" + '</li>');
 				}
